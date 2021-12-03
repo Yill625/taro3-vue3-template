bc4821261c75869490ebe1bf5adcd2c4ea48d73a
Ivan <1095036273@qq.com>
2021-12-03 11:13:49 +0800

    perf: 按照实际情况将组件引入主包还是分包的页面

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,6 @@
 import { createApp } from 'vue'
 import './app.scss'
-import {
-  Button,
-  Avatar,
-  Icon,
-  Dialog,
-  Cell,
-  CellGroup,
-  Address,
-  Elevator,
-  Popup
-} from '@nutui/nutui-taro'
+import { Icon } from '@nutui/nutui-taro'
 import '@nutui/nutui-taro/dist/style.css'
 
 import store, { key } from './store'
@@ -20,14 +10,6 @@ const App = createApp({
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 })
   .use(store, key)
-  .use(Button)
-  .use(Cell)
   .use(Icon)
-  .use(Address)
-  .use(Popup)
-  .use(Elevator)
-  .use(CellGroup)
-  .use(Avatar)
-  .use(Dialog)
 
 export default App

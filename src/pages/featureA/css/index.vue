35a8fa0c72c1e2a9830108f6e2699f881c2fb318
Ivan <1095036273@qq.com>
2021-12-20 13:49:10 +0800

    feat: 引入iconfont

--- src/pages/featureA/css/index.vue
+++ src/pages/featureA/css/index.vue
@@ -3,6 +3,13 @@
     测试超行省略fffffffffffffff
   </div>
   <p>1px</p>
+  <nut-icon
+    font-class-name="iconfont"
+    class-prefix="icon-weibiaoti-2_fuzhi"
+    name="lazhu"
+    size="30px"
+    color="red"
+  />
   <div class="van-hairline--top"></div>
 </template>
 

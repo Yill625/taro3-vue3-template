3064c5c9fdbb8bbb4440e212018514fb6a758cf0
Ivan <1095036273@qq.com>
2022-01-13 17:53:21 +0800

    feat: 注释ts

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,17 @@
+const fs = require('fs')
+const generateTSConfig = (stagedFilenames) => {
+  return (type) => {
+    console.log(type)
+    const tsconfig = fs.readFileSync('tsconfig.json', 'utf8')
+    console.log(tsconfig)
+    if (stagedFilenames.length === 0) return ''
+    tsconfig.include = stagedFilenames
+    fs.writeFileSync('tsconfig.lint.json', JSON.stringify(tsconfig))
+    return `${type} --noEmit --skipLibCheck  --project tsconfig.lint.json`
+  }
+}
+
 module.exports = {
   'src/**/*.{js,jsx,vue,ts,tsx}': ['yarn lint'],
-  'src/**/*.{vue,ts}': ['yarn tsc'],
+  // '*.{vue,ts}': [(fileName) => generateTSConfig(fileName)('vue-tsc')],
 }

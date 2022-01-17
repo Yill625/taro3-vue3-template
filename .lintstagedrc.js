94d18535a5ded6d1a2aff9adcd5443f4b2c0a7e6
Ivan <1095036273@qq.com>
2022-01-17 15:10:11 +0800

    feat: 修改 vue-tsc 验证为修改文件

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,17 +1,11 @@
 const fs = require('fs')
 const generateTSConfig = (stagedFilenames) => {
   return (type) => {
-    console.log(type)
-    const tsconfig = fs.readFileSync('tsconfig.json', 'utf8')
-    console.log(tsconfig)
-    if (stagedFilenames.length === 0) return ''
-    tsconfig.include = stagedFilenames
-    fs.writeFileSync('tsconfig.lint.json', JSON.stringify(tsconfig))
-    return `${type} --noEmit --skipLibCheck  --project tsconfig.lint.json`
+    return `${type} --noEmit --skipLibCheck`
   }
 }
 
 module.exports = {
   'src/**/*.{js,jsx,vue,ts,tsx}': ['yarn lint'],
-  // '*.{vue,ts}': [(fileName) => generateTSConfig(fileName)('vue-tsc')],
+  '*.{vue,ts}': [(fileName) => generateTSConfig(fileName)('vue-tsc')],
 }

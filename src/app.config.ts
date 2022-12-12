892c964fa38aa70055032e2dae7be07f4b93bb66
Ivan <1095036273@qq.com>
2022-12-12 20:40:15 +0800

    feat: 多小程序打包

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,5 +1,21 @@
+// 根据appId 打包到不同小程序去
+const index = process.env.npm_config_argv
+  ? JSON.parse(process.env.npm_config_argv).remain[process.env.NODE_ENV === 'development' ? 1 : 0]
+  : 'wx00af2d3955d230c2'
+const xcx = {
+  wx00af2d3955d230c2: {
+    page: ['pages/index/index'],
+    subpackages: [
+      {
+        root: 'pages/featureA',
+        pages: ['index/index', 'nutui/index', 'request/index', 'css/index'],
+      },
+    ],
+  },
+}
+
 export default defineAppConfig({
-  pages: ['pages/index/index'],
+  pages: xcx[index].page,
   window: {
     backgroundColor: '#fff',
     backgroundTextStyle: 'light',
@@ -7,10 +23,5 @@ export default defineAppConfig({
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black',
   },
-  subpackages: [
-    {
-      root: 'pages/featureA',
-      pages: ['index/index', 'nutui/index', 'request/index', 'css/index'],
-    },
-  ],
+  subpackages: xcx[index].subpackages,
 })
